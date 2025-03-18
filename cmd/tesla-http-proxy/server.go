74e20421cbf2ddb789aca7b5e830acb235b43e97
김다엘 <daelkdev@gmail.com>
2025-03-19 05:09:18 +0900

    remove self sign ssl:

--- cmd/tesla-http-proxy/server.go
+++ cmd/tesla-http-proxy/server.go
@@ -1,51 +1,11 @@
 package main
 
 import (
-	"crypto/ecdsa"
-	"crypto/elliptic"
-	"crypto/rand"
-	"crypto/x509"
-	"crypto/x509/pkix"
-	"encoding/pem"
-	"math/big"
-	"net"
 	"net/http"
-	"time"
 )
 
 // Since the proxy just listens on localhost, a self-signed certificate shouldn't present any
 // issues.
-func selfSignedCertificate() (certPEM []byte, keyPEM []byte, err error) {
-	cert := x509.Certificate{
-		SerialNumber: big.NewInt(2),
-		Subject: pkix.Name{
-			CommonName: "localhost",
-		},
-		IPAddresses: []net.IP{net.IPv4(127, 0, 0, 1), net.IPv6loopback},
-		NotBefore:   time.Now().Add(-time.Minute),
-		NotAfter:    time.Now().Add(time.Hour * 24 * 365 * 5),
-		KeyUsage:    x509.KeyUsageDigitalSignature,
-		ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth, x509.ExtKeyUsageClientAuth},
-		IsCA:        true,
-	}
-
-	skey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-	if err != nil {
-		return
-	}
-	certDER, err := x509.CreateCertificate(rand.Reader, &cert, &cert, &skey.PublicKey, skey)
-	if err != nil {
-		return
-	}
-	certPEM = pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
-
-	keyDER, err := x509.MarshalECPrivateKey(skey)
-	if err != nil {
-		return
-	}
-	keyPEM = pem.EncodeToMemory(&pem.Block{Type: "EC PRIVATE KEY", Bytes: keyDER})
-	return
-}
 
 func NewServer(addr string) *http.Server {
 
